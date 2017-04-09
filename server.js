57499ff46ace57110dfff36eb0281400c95f9e36
Andy F <andrew@cyberdreaming.net>
2017-04-09 10:29:34 -0700

    fix a todo

--- server.js
+++ server.js
@@ -2,12 +2,39 @@ var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+var todos = [{
+	id: 1,
+	description: 'Meet mom for lunch',
+	completed: false
+}, {
+	id: 2,
+	description: 'Go to market',
+	completed: false
+}, {
+	id: 3,
+	description: 'Feed the cat',
+	completed: false
+}];
+
+
 app.get('/', function (req, res) {
 	res.send('Todo API Root');
 });
 
+//  Get  /todos
+app.get('/todos', function (rec, res) {
+	res.json(todos);
+});
+
+//  GET /tods/:id
+
+
+
 app.listen(PORT, function () {
 	console.log('Express listening on port ' + PORT + '!')
 });
+
+
+
 
 
