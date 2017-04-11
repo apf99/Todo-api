159ef7634c644cc59467b445f7f27dee0d569c07
Andrew Friedman <andrew@cyberdreaming.net>
2017-04-11 08:58:57 -0700

    Add variable playground area

---
+++ variable-playground.js
@@ -0,0 +1,36 @@
+// var person = {
+// 	name: 'Andrew',
+// 	age: 21
+// };
+
+// function updatePerson (obj) {
+// 	// obj = {
+// 	// 	name: 'Andrew',
+// 	// 	age: 24
+// 	// };
+
+// 	obj.age = 24;
+
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+
+
+//  Array Example
+
+var grades = [15, 37];
+
+function addGrade (grades) {
+	//grades.push(24);
+	grades = [12, 33, 99]
+
+	debugger;
+	
+}
+
+
+addGrade(grades);
+console.log(grades);
+
