277e76a1b8f8dc29dce9a3556328b4c4b9fa72e9
Andrew Friedman <andrew@cyberdreaming.net>
2017-04-19 10:40:23 -0700

    Convert DELETE /todos/:is to sequelize

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.get('/todos/:id', function (req, res) {
 			res.status(404).send();
 		}
 	}, function (e) {
-		res.satus(500).send();
+		res.status(500).send();
 	});
 });
 
@@ -67,14 +67,22 @@ app.post('/todos', function (req, res) {
 
 app.delete('/todos/:id', function (req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo = _.findWhere(todos, {id: todoId});
 
-	if (matchedTodo) {
-		todos = _.without(todos, matchedTodo);
-		res.json(matchedTodo);
-	} else {
-		res.status(404).json({'error': 'no todo found with that id'});
-	}
+	db.todo.destroy({
+		where: {
+			id: todoId
+		}
+	}).then(function (rowsDeleted) {
+		if (rowsDeleted === 0) {
+			res.status(404).json({
+				error: 'No todo with id'
+			});
+		} else {
+			res.status(204).send();
+		}
+	}, function (e) {
+		res.status(500).send();
+	});
 });
 
 // PUT /todos/:id
